aa2013de0c4c35f869631c7c29297c7e548c827d
Krishaghetiya <krishaghetiya2006@gmail.com>
2026-02-07 19:32:57 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,18 +1,13 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/nodeFinal');
+mongoose.connect(
+  "mongodb+srv://krishaghetiya2006_db_user:Sangit@cluster0.qksa3bu.mongodb.net/?appName=Cluster0"
+);
 
 const connectDB = mongoose.connection;
 
-connectDB.on('connected',(err)=>{
-    if (err) {
-        console.log("Database is not connected...");
-        return false;
-    }
-
-    console.log("Database is connected successfully")
-})
+connectDB.once("connected", () => {
+  console.log("MongoDB Atlas Connected");
+});
 
 module.exports = {connectDB};
-
-
